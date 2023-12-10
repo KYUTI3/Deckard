93584ebd7e2f750b48cfec64c58da41132ea1b68
Ansh-123 <63263061+Ansh-123@users.noreply.github.com>
2023-12-09 18:29:01 -0800

    Update main.py
    Added attendance tracking

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ import sqlite3
 
 
 # generate event attendance table
-def generate_event_table(cursor, EventName):
+def generate_event_table(cursor, eventName, eventCode):
+    # create table of attendance for this event
     table = """ CREATE TABLE IF NOT EXISTS %s(
-                User CHAR(25) NOT NULL,
+                User CHAR(25) NOT NULL
             );
-             """ % EventName
+             """ % eventName
     cursor.execute(table)
+    cursor.execute('SELECT * FROM Events WHERE Name ="%s"' % eventName)
+    event = cursor.fetchone()
+
+    # add event to table of events
+    if event is None:
+        cursor.execute("INSERT INTO Events VALUES ('%s', '%s')" % (eventName, eventCode))
 
 # initialize flask
 app = Flask(__name__)
@@ -23,7 +30,7 @@ connection = sqlite3.connect('data.db')
 cursor_obj = connection.cursor()
 #cursor_obj.execute("DROP TABLE IF EXISTS attend")
 
-table = """ CREATE TABLE IF NOT EXISTS users(
+table = """ CREATE TABLE IF NOT EXISTS Users(
             User CHAR(25) NOT NULL,
             Email VARCHAR(255) NOT NULL,
             Password CHAR(25) NOT NULL,
@@ -39,10 +46,10 @@ table = """ CREATE TABLE IF NOT EXISTS Events(
          """
 cursor_obj.execute(table)
 
-connection.commit()
-
 # example event
-generate_event_table(cursor_obj, "hackathon")
+generate_event_table(cursor_obj, "Hackathon", 1234)
+
+connection.commit()
 connection.close()
 
 
@@ -55,7 +62,7 @@ def login():
         password = request.form['password']
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
-        cursor.execute('SELECT * FROM users WHERE Email ="%s" AND Password = "%s"' % (email, password))
+        cursor.execute('SELECT * FROM Users WHERE Email ="%s" AND Password = "%s"' % (email, password))
         user = cursor.fetchone()
         connection.close()
 
@@ -64,20 +71,55 @@ def login():
             session["name"] = user[0]
             session["email"] = email
             session["password"] = password
-            # todo redirect to attend
-            return render_template('user.html', message=message)
+            session["score"] = user[3]
+            # todo redirect to attend (done)
+            return render_template('user.html', message=message, score=session["score"])
         else:
             message = 'Please enter correct email / password !'
     return render_template('login.html', message=message)
 
 
 # Make function attendance
-@app.route('/attend')
+@app.route('/attend', methods=['GET', 'POST'])
 def attend():
-    session["name"] = None
-    session["email"] = None
-    session["password"] = None
-    return redirect(url_for('login'))
+    message = ''
+    # make sure user is logged in
+    if session["name"] is None:
+        return redirect(url_for('login'))
+    else:
+        # check that form data is available
+        if request.method == 'POST' and 'Name' in request.form and 'Code' in request.form:
+            eventName = request.form['Name']
+            eventCode = request.form['Code']
+
+            # verify event name and code
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor()
+            cursor.execute('SELECT * FROM Events WHERE Name ="%s" AND Code = "%s"' % (eventName, eventCode))
+            event = cursor.fetchone()
+            # check if event name and code are valid
+            if event:
+                # check if person is already registered in the event
+                cursor.execute('SELECT * FROM %s WHERE User ="%s"' % (eventName, session['name']))
+                person = cursor.fetchone()
+                print(person)
+                if person is None:
+                    # add person into event attendance and update their score
+                    cursor.execute("INSERT INTO %s VALUES ('%s')" % (eventName, session["name"]))
+                    cursor.execute('''UPDATE Users SET Score = %s WHERE User = "%s";''' % (session["score"]+1, session["name"]))
+                    connection.commit()
+                    session["score"] = session["score"] + 1
+                    connection.close()
+                    message = "success"
+                    return render_template('user.html', message=message, score=session["score"])
+                else:
+                    message = 'You have already attended this event'
+                    return render_template('user.html', message = message, score=session["score"])
+            else:
+                connection.close()
+                message = "Event name or password incorrect"
+                return render_template('attend.html', message=message)
+        return render_template('attend.html', message=message)
 
 
 # Make function for logout session
@@ -99,17 +141,17 @@ def register():
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        cursor.execute('SELECT * FROM users WHERE Email ="%s"' % (email))
+        cursor.execute('SELECT * FROM Users WHERE Email ="%s"' % (email))
         user = cursor.fetchone()
 
         # if user has account redirect to login
         if user:
             message = 'you already have an account, please login'
-            return redirect(url_for('login'))
+            render_template('login.html', message=message)
 
         # add user account
         else:
-            cursor.execute("INSERT INTO users VALUES ('%s', '%s', '%s', 0)" % (userName, email, password))
+            cursor.execute("INSERT INTO Users VALUES ('%s', '%s', '%s', 0)" % (userName, email, password))
             message = "done registering, please login"
         connection.commit()
         connection.close()
